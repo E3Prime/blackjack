5dcc56df9690c4e6bcab2828b379e570cf24e43a
E3Prime <primesoftware3@gmail.com>
2026-02-14 21:54:06 -0700

    still experimenting

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,5 @@
-import {animate, transform} from "motion";
+import {animate, delay, stagger} from "motion";
 
-const titleScreenElem = document.getElementById("titleScreen") as HTMLElement;
-const startGameBtn = titleScreenElem.lastElementChild as HTMLButtonElement;
-const gameScreenElem = document.getElementById("gameScreen") as HTMLElement;
+const box = document.getElementById("box");
 
-startGameBtn.addEventListener("click", initializeGame);
-
-function initializeGame() {
-  animate(startGameBtn, {y: "100vh", opacity: 0}, {duration: 0.75, ease: "easeIn"}).then(() => (startGameBtn.disabled = true));
-}
+animate(box, {skewX: 1});
